4e37a2c70e424512ea5b3ac4ea407b6d87a04925
Guillermo San Martín <guille.seba0802@gmail.com>
2022-12-17 11:24:57 -0300

    textures added

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,16 +1,19 @@
 import { usePlane } from "@react-three/cannon";
+import { grassTexture } from "../images/textures";
 
 export default function Ground() {
 
     const [ref] = usePlane(() => ({
-        rotation: [0, 0, 0],
-        position: [0, 0, 0]
+        rotation: [-Math.PI / 2, 0, 0],
+        position: [0, -1, 0]
     }))
+
+    grassTexture.repeat.set(100, 100)
 
   return (
     <mesh ref={ref}>
         <planeBufferGeometry attach='geometry' args={[100, 100]} />
-        <meshStandardMaterial attach='material' color='green' />
+        <meshStandardMaterial attach='material' map={grassTexture} />
     </mesh>
   )
 }
